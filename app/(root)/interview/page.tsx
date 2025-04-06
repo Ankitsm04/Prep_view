5ae00f819bfc04969fbcced479ad578af69786ed
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2025-04-06 19:27:34 +0530

    Implemented Interview Generation

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Agent from '@/components/Agent'
 
 const page = () => {
+    const api_key = process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN;
+    console.log(api_key);
   return (
     <>
         <h3>Interview Generation</h3>
